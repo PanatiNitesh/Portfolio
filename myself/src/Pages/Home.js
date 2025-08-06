7a9085b4ddb931a101b636ecb04e16308a5fab99
PanatiNitesh <niteshreddy242005@gmail.com>
2025-08-06 22:25:24 +0530

    updated home.js

--- myself/src/Pages/Home.js
+++ myself/src/Pages/Home.js
@@ -58,26 +58,27 @@ function Home() {
 
   return (
     <main className="container mx-auto max-width section md:flex justify-between items-center">
-      <div>
-        <h1
-          ref={h11}
-          className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
-        >
-          Hi,👋<br></br>I'm
-        </h1>
-        <h1
-          ref={h12}
-          className="text-2xl bg-clip-text bg-gradient text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold"
-        >
-          {name}
-        </h1>
-        <h2
-          ref={h13}
-          className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
-        >
-          {tagline}
-        </h2>
-      </div>
+  <div>
+    <h1
+      ref={h11}
+      className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+    >
+      Hi, 👋 I'm{" "}
+      <span
+        ref={h12}
+        className="bg-clip-text bg-gradient text-transparent"
+      >
+        {name}
+      </span>
+    </h1>
+    <h2
+      ref={h13}
+      className="text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold"
+    >
+      {tagline}
+    </h2>
+  </div>
+
 <div className="mt-5 md:mt-0 flex justify-center md:justify-end">
   <img
     ref={myimageref}
