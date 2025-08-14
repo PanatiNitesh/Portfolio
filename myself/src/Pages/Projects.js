356334dfa4960e7ec380bb366d36d11b0b891b10
PanatiNitesh <niteshreddy242005@gmail.com>
2025-08-14 19:18:44 +0530

    updated pages/project.js

--- myself/src/Pages/Projects.js
+++ myself/src/Pages/Projects.js
@@ -6,13 +6,12 @@ function Projects() {
   return (
     <main className="container mx-auto max-width pt-10 mb-20">
       <section>
-       <h1 className="text-3xl md:text-5xl xl:text-6xl font-extrabold mt-4 mb-10 
-               bg-clip-text text-transparent 
-               bg-gradient-to-r from-blue-500 to-purple-500 
-               border-b-4 border-blue-500 w-max pb-2">
-  Projects
-</h1>
-
+        <h1 className="text-3xl md:text-5xl xl:text-6xl font-extrabold mt-4 mb-10 
+                 bg-clip-text text-transparent 
+                 bg-gradient-to-r from-blue-500 to-purple-500 
+                 border-b-4 border-blue-500 w-max pb-2">
+          Projects
+        </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-10 mt-6">
           {projectDetails.map(
             ({ title, image, description, techstack, previewLink, githubLink }, index) => (
@@ -29,6 +28,7 @@ function Projects() {
           )}
         </div>
       </section>
+
     </main>
   );
 }
